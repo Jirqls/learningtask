30fcab4cef32a25866df68658822b3163cebfa58
Jirqls <100014589+Jirqls@users.noreply.github.com>
2022-03-13 13:58:38 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>My Home Page</title>
+    </head>
+  <body>
+    <h1>Welcome to My Website! I am Jill Anne L. Orquiales<3</h1>
+    <ul>
+      <li><a href="index.html">HOME</a></li> 
+      <li><a href="Index.html">Learning Task 3</a></li>
+    </ul>
+  </body>
+</html>
