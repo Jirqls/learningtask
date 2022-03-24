690e8357f06aed5a591d31a69014f15f8d3b418b
Jirqls <100014589+Jirqls@users.noreply.github.com>
2022-03-24 22:48:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <h1>Welcome to My Website! I am Jill Anne L. Orquiales<3</h1>
     <ul>
       <li><a href="index.php">HOME</a></li> 
-      <li><a href="home.html">Learning Task2</a></li>
+      <li><a href="cookingrecipes.html">Learning Task2</a></li>
       <li><a href="index.html">Learning Task3</a></li>
       <li><a href="index2.html">CLE1MT</a></li>
     </ul>
