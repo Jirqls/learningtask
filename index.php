0b4dccb6e9ac04852922435570c33a4a2335b543
Jirqls <100014589+Jirqls@users.noreply.github.com>
2022-03-31 05:31:23 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <ul>
       <li><a href="index.php">HOME</a></li> 
       <li><a href="cookingrecipes.html">Learning Task2</a></li>
-      <li><a href="index.html">Learning Task3</a></li>
+      <li><a href="Index.html">Learning Task3</a></li>
       <li><a href="index2.html">CLE1MT</a></li>
     </ul>
   </body>
